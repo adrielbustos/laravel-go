9037f0f545206e680e325c22eaeaeac3740d3325
Adriel Bustos <adriel.bustos01@gmail.com>
2022-07-29 16:50:08 -0300

    add session control with coockies

--- celeritas/celeritas.go
+++ celeritas/celeritas.go
@@ -9,9 +9,11 @@ import (
 	"time"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"github.com/tsawler/celeritas/render"
+	"github.com/tsawler/celeritas/session"
 )
 
 const version = "1.0.0"
@@ -27,13 +29,16 @@ type Celeritas struct {
 	RootPath string
 	Routes   *chi.Mux
 	Render   *render.Render
+	Session  *scs.SessionManager
 	JetViews *jet.Set
 	config   config
 }
 
 type config struct {
-	port     string
-	renderer string
+	port        string
+	renderer    string
+	cookie      cookieConfig
+	sessionType string
 }
 
 // New reads the .env file, creates our application config, populates the Celeritas type with settings
@@ -72,7 +77,27 @@ func (c *Celeritas) New(rootPath string) error {
 	c.config = config{
 		port:     os.Getenv("PORT"),
 		renderer: os.Getenv("RENDERER"),
+		cookie: cookieConfig{
+			name:     os.Getenv("COOKIE_NAME"),
+			lifetime: os.Getenv("COOKIE_LIFETIME"),
+			persist:  os.Getenv("COOKIE_PERSISTS"),
+			secure:   os.Getenv("COOKIE_SECURE"),
+			domain: os.Getenv("COOKIE_DOMAIN"),
+		},
+		sessionType: os.Getenv("SESSION_TYPE"),
 	}
+
+	// create session
+
+	sess := session.Session {
+		CookieLifetime: c.config.cookie.lifetime,
+		CookiePersist: c.config.cookie.persist,
+		CookieName: c.config.cookie.name,
+		SessionType: c.config.sessionType,
+		CookieDomain: c.config.cookie.domain,
+	}
+
+	c.Session = sess.InitSession()
 
 	var views = jet.NewSet(
 		jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
--- myapp/go.mod
+++ myapp/go.mod
@@ -5,12 +5,13 @@ go 1.17
 replace github.com/tsawler/celeritas => ../celeritas
 
 require (
+	github.com/CloudyKit/jet/v6 v6.1.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/tsawler/celeritas v0.0.0
 )
 
 require (
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
-	github.com/CloudyKit/jet/v6 v6.1.0 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 )
