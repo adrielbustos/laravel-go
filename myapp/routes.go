3becedacb31f8bcd3b99532f1e24cea7b04a0ae8
Adriel Bustos <adriel.bustos01@gmail.com>
2022-08-05 09:12:53 -0300

    connect to a postgres database

--- myapp/routes.go
+++ myapp/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,12 +10,27 @@ import (
 func (a *application) routes() *chi.Mux {
 	// middleware must come before any routes
 
-
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+
+	a.App.Routes.Get("/test-database", func(w http.ResponseWriter, r *http.Request) {
+		query := "select id, name from users where id = 1"
+		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
+
+		var id int
+		var name string
+
+		err := row.Scan(&id, &name)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		fmt.Fprintf(w, "%d %s", id, name)
+	})
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
